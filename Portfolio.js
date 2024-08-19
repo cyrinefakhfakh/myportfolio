830a9ba4e688c3807b815af2ad3d4354f7a18b37
Cyrine Fk <119887703+cyrinefakhfakh@users.noreply.github.com>
2024-08-19 23:21:36 +0100

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -4,6 +4,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 
+
+
+
 function Portfolio() {
   return (
     <div>
